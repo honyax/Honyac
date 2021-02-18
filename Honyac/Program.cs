b6e0c7827d817b94d66a804e13ae3a368393c3aa
honyax <honyax@gmail.com>
2021-02-19 02:30:59 +0900

    feat: ステップ2：加減算のできるコンパイラの作成

--- Honyac/Program.cs
+++ Honyac/Program.cs
@@ -5,6 +5,65 @@ namespace Honyac
 {
     public class Program
     {
+        private string SourceCode { get; set; }
+
+        public Program(string sourceCode)
+        {
+            this.SourceCode = sourceCode;
+        }
+
+        public string Execute()
+        {
+            // 50+10-25
+            var sb = new StringBuilder();
+            sb.AppendLine($".intel_syntax noprefix");
+            sb.AppendLine($".globl main");
+            sb.AppendLine($"main:");
+
+            var index = 0;
+            while (index < SourceCode.Length)
+            {
+                string calc;
+                if (index == 0)
+                {
+                    calc = "mov";
+                }
+                else
+                {
+                    switch (SourceCode[index])
+                    {
+                        case '+':
+                            calc = "add";
+                            break;
+                        case '-':
+                            calc = "sub";
+                            break;
+                        default:
+                            throw new ArgumentException($"Invalid SourceCode:{SourceCode}");
+                    }
+                    index++;
+                }
+
+                var value = 0;
+                for (; index < SourceCode.Length; index++)
+                {
+                    if (char.IsDigit(SourceCode[index]))
+                    {
+                        value = value * 10 + int.Parse(SourceCode[index].ToString());
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                sb.AppendLine($"  {calc} rax, {value}");
+            }
+
+            sb.AppendLine($"  ret");
+
+            return sb.ToString();
+        }
+
         public static void Main(string[] args)
         {
             if (args.Length != 1)
@@ -13,13 +72,8 @@ namespace Honyac
                 return;
             }
 
-            var sb = new StringBuilder();
-            sb.AppendLine($".intel_syntax noprefix");
-            sb.AppendLine($".globl main");
-            sb.AppendLine($"main:");
-            sb.AppendLine($"  mov rax, {int.Parse(args[0])}");
-            sb.AppendLine($"  ret");
-            Console.Write(sb.ToString());
+            var p = new Program(args[0]);
+            Console.Write(p.Execute());
         }
     }
 }
--- HonyacTests/UnitTest1.cs
+++ HonyacTests/UnitTest1.cs
@@ -29,5 +29,29 @@ namespace HonyacTests
                 Assert.AreEqual(output.ToString(), outTest.ToString());
             }
         }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var inTest = "80-95+50-20";
+            var outTest = new StringBuilder();
+            outTest.AppendLine(".intel_syntax noprefix");
+            outTest.AppendLine(".globl main");
+            outTest.AppendLine("main:");
+            outTest.AppendLine("  mov rax, 80");
+            outTest.AppendLine("  sub rax, 95");
+            outTest.AppendLine("  add rax, 50");
+            outTest.AppendLine("  sub rax, 20");
+            outTest.AppendLine("  ret");
+
+            using (var output = new StringWriter())
+            {
+                Console.SetOut(output);
+
+                Program.Main(new string[] { inTest });
+
+                Assert.AreEqual(output.ToString(), outTest.ToString());
+            }
+        }
     }
 }
